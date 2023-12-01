c21148a895b0b8e2b015e7c0656cee21fc72423d
Frederico Tavares <fredericotp@hotmail.com>
2023-12-01 13:50:59 -0300

    Fix: Tabela posts squema.sql

--- fullstack/db/schema.sql
+++ fullstack/db/schema.sql
@@ -1,16 +1,17 @@
 USE `main`;
 
-CREATE TABLE posts (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    author_id INT NOT NULL,
-    post_text VARCHAR(200)
-);
-
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     full_name VARCHAR(100),
     user_email VARCHAR(100) NOT NULL,
     user_password VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE posts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    author_id INT NOT NULL,
+    post_text VARCHAR(200),
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
